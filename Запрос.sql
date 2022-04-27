b726a2833046fbd0d5588c9fd916ffaaff3cb130
JustExsisting <vaxrushev03@gmail.com>
2022-04-27 12:04:23 +0400

    Update Запрос.sql

--- Запрос.sql
+++ Запрос.sql
@@ -1,3 +1,3 @@
 SELECT Surname, Name, Quantity FROM sales JOIN sellers on sales.IDSales = sellers.ID
-WHERE (Date >= '2013-10-01' and Date <= '2013-10-07')
+WHERE (Date >= '2013-10-01' and Date < '2013-10-08')
 ORDER BY sellers.Surname, sellers.Name;
